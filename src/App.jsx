6ef02473d4f96e49a273d4e9d14b6ff03df3a7df
Sujal Acharya <sujalacharya567@gmail.com>
2024-03-17 00:43:26 -0400

    Navbar almost done, just needs more styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,6 @@ function App() {
     <>
     <main>
     <Navbar/>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum veniam sint excepturi iste quibusdam autem sequi, expedita ratione velit aspernatur! Accusamus optio ullam illum, odit sequi pariatur impedit commodi voluptatibus!
-      Earum eos, cum doloremque consequuntur rem soluta quaerat veniam modi quos. Harum, voluptate facere expedita vero repellendus autem. Non iusto eos quibusdam illo quae? Debitis, deserunt? Et eaque ab quaerat.
-      Ipsum veniam odit nostrum quibusdam, quaerat, deserunt autem expedita vel enim illum voluptatem quos commodi exercitationem odio cum, nulla ullam! Obcaecati molestias, nisi illum necessitatibus quis quasi hic quo neque!
-      Nulla nihil eos est sed corrupti cupiditate hic! Harum nobis ea nesciunt atque sit similique neque ipsum voluptate vero, consequuntur repellat et pariatur iusto rem modi accusantium illum id. Inventore.
-      Vitae sed voluptatibus a culpa amet quo, fugit pariatur ipsam! Quos quaerat eos, unde mollitia, corporis modi omnis enim porro nulla iure deleniti accusamus veniam laboriosam repellendus. Porro, itaque beatae!
-      Repellendus voluptatum maiores cupiditate exercitationem nam, odit eaque dolore veniam sapiente quaerat molestias asperiores voluptates nemo ipsum vel eveniet minima. Ex ipsa repudiandae ullam iusto nobis, debitis cum eveniet dolore.
-      Voluptatum iusto, corrupti laboriosam ab aspernatur sequi harum quo mollitia deserunt quod. Minus quidem earum, sit dolorem, quae, soluta incidunt vitae ducimus similique maxime deleniti quo placeat asperiores esse consequuntur?
-      Eveniet, numquam reprehenderit illo reiciendis, optio nobis, provident architecto eaque nulla delectus atque! Eveniet cum repudiandae amet atque quidem quo necessitatibus sapiente, provident veritatis officiis sequi aspernatur, molestiae inventore praesentium!
-      Enim tempora dicta dolorem laborum eaque hic corporis tenetur ad saepe fugit aspernatur omnis maiores quas, repellat numquam. Itaque magnam deleniti vel minima omnis fugiat officiis quod consectetur ducimus sapiente.
-      Voluptates amet, placeat modi voluptas pariatur sit! Eligendi sit porro, labore voluptas eaque pariatur? Repudiandae laboriosam accusamus cupiditate commodi quas pariatur fugiat dicta unde repellat. Expedita fugit ab commodi ratione!
-      </h1>
     </main>
     </>
   )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,33 +1,40 @@
+import  { useState } from 'react';
 import "./Navbar.css";
 import Button from "./SillyComponents/Button";
 
+export default function Navbar() {
+    const [showDropdown, setShowDropdown] = useState(false);
 
-export default function Navbar(){
-    return(
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    return (
         <>
             <div className="navbar">
                 <div className="mylogo">
                     Sujal Acharya
                 </div>
                 <ul>
-                    <li><Button text="Journey" url={"https://www.google.com"}/></li>
-                    <li><Button text="About Me"/></li>
-                    <li><Button text="Playground"/></li>
+                    <li><Button text="Journey" url={"https://www.google.com"} /></li>
+                    <li><Button text="About Me" /></li>
+                    <li><Button text="Playground" /></li>
                 </ul>
-                <div className="toggle_btn">
-                    <span class="material-symbols-outlined">
-                            menu 
+                <div className="toggle_btn" onClick={toggleDropdown}>
+                    <span className="material-symbols-outlined">
+                        menu
                     </span>
                 </div>
             </div>
-            <div className="dropdown_menu">
-                <ul>
-                    <li><Button text="Journey" url={"https://www.google.com"}/></li>
-                    <li><Button text="About Me"/></li>
-                    <li><Button text="Playground"/></li>
-                </ul>
-            </div>
-
+            {showDropdown && (
+                <div className="dropdown_menu">
+                    <ul>
+                        <li><Button text="Journey" url={"https://www.google.com"} /></li>
+                        <li><Button text="About Me" /></li>
+                        <li><Button text="Playground" /></li>
+                    </ul>
+                </div>
+            )}
         </>
-    )
+    );
 }
